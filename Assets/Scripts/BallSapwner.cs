0d01ef0342e294261172329ccd46f246dec76bc6
unknown <mohamadghgh.273@gmail.com>
2024-04-13 00:42:55 +0330

    better main menu design

--- Assets/Scripts/BallSapwner.cs
+++ Assets/Scripts/BallSapwner.cs
@@ -1,31 +1,58 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class BallSapwner : MonoBehaviour
 {
 
+    private const int maxProduce = 25;
+
+    private int ballCounter;
+    
     public GameObject smallBall;
+    
+    public GameObject bigBall;
+
+    public Vector3 forceDirection;
+
+    private int power;
+
+    public Material[] materials;
+
     
     
     // Start is called before the first frame update
     private void Start()
     {
-        
+        StartCoroutine(SpawnBall());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     IEnumerator SpawnBall()
     {
-        while (true)
+        while (ballCounter <=maxProduce)
         {
-            yield return new WaitForSeconds(0.4f);
+            yield return new WaitForSeconds(1.2f);
+            int randNum = Random.Range(0, 3);
+            power = Random.Range(0, 70);
+            int randMat = Random.Range(0, materials.Length);
+            switch (randNum)
+            {
+                case 0:  // spawning small ball
+                    GameObject SBall = Instantiate(smallBall, transform.position, Quaternion.identity);
+                    SBall.GetComponent<MeshRenderer>().material = materials[randMat];
+                    SBall.GetComponent<Rigidbody>().AddForce(forceDirection * power,ForceMode.Impulse);
+                    ballCounter++;
+                    break;
+                case 1: // spawning big ball
+                    GameObject XBall = Instantiate(bigBall, transform.position, Quaternion.identity);
+                    XBall.GetComponent<MeshRenderer>().material = materials[randMat];
+                    XBall.GetComponent<Rigidbody>().AddForce(forceDirection * power,ForceMode.Impulse);
+                    ballCounter++;
+                    break;
+                case 2:  // nothing happens
+                    break;
+                    
+            }
+            
             
         }
     }
